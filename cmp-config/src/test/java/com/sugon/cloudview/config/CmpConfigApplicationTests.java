b0b2a5bba8f8c7e9fd3e9bbf6f5e8039821b1a2d
Unknown <unknown@example.com>
2019-07-11 19:11:10 +0800

    springcloud-config配置文件

---
+++ cmp-config/src/test/java/com/sugon/cloudview/config/CmpConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sugon.cloudview.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmpConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
